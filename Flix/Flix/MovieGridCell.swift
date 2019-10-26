52681793f0ba59e293fbd8252bcf0db03829ad2e
guilleeh <guilleeh@uci.edu>
2019-10-26 16:50:09 -0700

    add tab var view controller

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Guillermo Hernandez on 10/26/19.
+//  Copyright © 2019 Guillermo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
